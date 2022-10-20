8ea8bbd1d4e10daa782d2650cc9f903e6518607e
hammadalimughal <hammmad19ali@gmail.com>
2022-10-20 22:32:18 +0500

    converting to html

--- index.php
+++ index.php
@@ -1,7 +1,70 @@
-<?php
-include 'includes/header.php';
-$page = 'home';
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Home </title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css.map" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.min.css.map" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="images/favicon.png" />
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="css/cropper.css" /> -->
+    <link rel="stylesheet" type="text/css" href="css/crop.css" />
+    <link href="https://site-assets.fontawesome.com/releases/v6.0.0/css/all.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <header class="header-main">
+        <div class="top-header">
+            <h6>Low Price Guarantee.* Free Shipping. Free Returns. Free Design Advice. </h6>
+            <a href="javascript:;">View Details</a>
+        </div>
+        <div class="container-fluid position-relative">
+            <a class="header-logo" href="javascript:;">
+                <img class="img-fluid" src="images/logo.png" alt="">
+            </a>
+            <div class="mid-header">
+                <div class="search-field">
+                    <input type="text" placeholder="Ex: 24 X 32, Picasso, Blue Abstract, Kitchen, etc.">
+                    <button><i class="fa-regular fa-magnifying-glass"></i></button>
+                </div>
+                <ul class="mid-ul">
+                    <li><a class="link-icons" href="javascript:;"><i class="fa-solid fa-question"></i></a></li>
+                    <li><a class="link-icons" href="javascript:;"><i class="fa-solid fa-messages"></i></a></li>
+                    <li><a class="link-icons" href="javascript:;"><i class="fa-solid fa-heart"></i></a></li>
+                    <li><a class="link-icons" href="javascript:;"><i class="fa-solid fa-user"></i></a></li>
+                    <li><a class="bar" href="javascript:;"><span>Sign Up</span></a></li>
+                    <li><a href="javascript:;"><span>Log In</span></a></li>
+                    <li><a class="link-icons" href="javascript:;"><i class="fa-light fa-cart-shopping"></i></a></li>
+                </ul>
+            </div>
+            <div class="bottom-header">
+                <ul class="bottom-ul">
+                    <li><a href="javascript:;"><span>new!</span> gallery walls</a></li>
+                    <li><a href="javascript:;">new arrivals</a></li>
+                    <li><a href="javascript:;">best sellers</a></li>
+                    <li><a href="javascript:;">trends</a></li>
+                    <li><a href="javascript:;">artists</a></li>
+                    <li><a href="javascript:;">subjects</a></li>
+                    <li><a href="javascript:;">photography</a></li>
+                    <li><a href="javascript:;">design inspiration</a></li>
+                    <li><a href="javascript:;">frame my photo</a></li>
+                    <li><a href="javascript:;">get $25</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
 <main>
     <section class="step img-upload-sec active">
         <div class="upload-nav">
@@ -339,7 +402,17 @@ $page = 'home';
         </div>
     </section>
 </main>
-<?php
-include 'includes/footer.php';
-$page = 'home';
-?>
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- <script src="js/cropper.js"></script> -->
+<script src="js/crop.js"></script>
+<script type="text/javascript" src="js/slick.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/0.1.12/wow.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/custom.js"></script>
+</script>
+</body>
+
+</html>
